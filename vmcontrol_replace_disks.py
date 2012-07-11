5758fd929a06b51cd89b1b22cf14d468b6383378
Manabu Ori <manabu.ori+github@gmail.com>
2012-07-11 14:32:00 +0900

    update.

--- vmcontrol_replace_disks.py
+++ vmcontrol_replace_disks.py
@@ -7,6 +7,22 @@ import re
 from optparse import OptionParser, OptionValueError
 from lxml import etree
 
+def get_vm_disks(vm):
+	virsh_dumpxml = "virsh dumpxml %s" % vm;
+	file = os.popen(virsh_dumpxml)
+	xml = etree.parse(file, parser=etree.XMLParser())
+	disks_elem = xml.xpath("/domain/devices/disk[@device='disk']")
+	disks = []
+	for disk_elem in disks_elem:
+		wwid_fullpath = disk_elem.xpath(".//source")[0].attrib['dev']
+		regexp = re.compile('^.*/');
+		wwid = regexp.sub('', wwid_fullpath)
+		realdev = os.path.realpath(wwid_fullpath)
+		dev = os.stat(realdev).st_rdev
+		obj = {'wwid':wwid, 'realdev':realdev, 'majmin':(os.major(dev), os.minor(dev))}
+		disks.append(obj)
+	return disks
+
 def build_dev2mpath():
 	dev2mpath = {}
 	dmsetup_list = "dmsetup ls | sort"
@@ -34,44 +50,24 @@ def build_dev2mpath():
 					dev2mpath[majmin] = mdev
 	return dev2mpath
 
-#def build_mpath2wwid(mpath):
-#	mpath2wwid = {}
-#	dmsetup_info = "dmsetup info %s" % mpath
-#	for uuid
-
-usage = "Usage: %s [--sed | --dumpxml] %s" % (sys.argv[0], "vm")
-
-parser = OptionParser(usage)
-parser.add_option("-s", "--sed", action="store_true", dest="sed_flag")
-parser.add_option("-d", "--dumpxml", action="store_true", dest="dumpxml_flag")
-parser.add_option("-w", "--wwid", action="store_true", dest="wwid_flag")
-parser.add_option("-m", "--mpath", action="store_true", dest="mpath_flag")
-(options, args) = parser.parse_args()
-
-if len(args) == 0:
-	print usage
-	sys.exit(1)
-
-vm = args[0]
-virsh_dumpxml = "virsh dumpxml %s" % vm;
-file = os.popen(virsh_dumpxml)
-xml = etree.parse(file, parser=etree.XMLParser())
-r = xml.getroot()
-e = xml.xpath("/domain/devices/disk/source")
-disks_elem = xml.xpath("/domain/devices/disk[@device='disk']")
-disks = []
-for disk_elem in disks_elem:
-	wwid_fullpath = disk_elem.xpath(".//source")[0].attrib['dev']
-	regexp = re.compile('^.*/');
-	wwid = regexp.sub('', wwid_fullpath)
-	realdev = os.path.realpath(wwid_fullpath)
-	dev = os.stat(realdev).st_rdev
-	obj = {'wwid':wwid, 'realdev':realdev, 'majmin':(os.major(dev), os.minor(dev))}
-	disks.append(obj)
-
-dev2mpath = build_dev2mpath()
-
 if __name__ == '__main__':
+	usage = "Usage: %s [--sed | --dumpxml] %s" % (sys.argv[0], "vm")
+
+	parser = OptionParser(usage)
+	parser.add_option("-s", "--sed", action="store_true", dest="sed_flag")
+	parser.add_option("-d", "--dumpxml", action="store_true", dest="dumpxml_flag")
+	parser.add_option("-w", "--wwid", action="store_true", dest="wwid_flag")
+	parser.add_option("-m", "--mpath", action="store_true", dest="mpath_flag")
+	(options, args) = parser.parse_args()
+
+	if len(args) == 0:
+		print usage
+		sys.exit(1)
+
+	vm = args[0]
+	disks = get_vm_disks(vm)
+	dev2mpath = build_dev2mpath()
+
 	if options.sed_flag:
 		print "sed", 
 		for disk in disks:
